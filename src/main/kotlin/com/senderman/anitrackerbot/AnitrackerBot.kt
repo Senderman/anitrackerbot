be3b34e67581fd80701a1086c5a9a5d7f7dec28b
Fyodor Doletov <doletov.fyodor@yandex.ru>
2020-04-23 19:37:42 +0300

    add unpack config

--- src/main/kotlin/com/senderman/anitrackerbot/AnitrackerBot.kt
+++ src/main/kotlin/com/senderman/anitrackerbot/AnitrackerBot.kt
@@ -4,12 +4,38 @@ import com.annimon.tgbotsmodule.BotHandler
 import com.annimon.tgbotsmodule.BotModule
 import com.annimon.tgbotsmodule.beans.Config
 import com.annimon.tgbotsmodule.services.YamlConfigLoaderService
+import java.io.File
+import java.io.FileOutputStream
 
 class AnitrackerBot : BotModule {
+
+    private val configDir = File("botConfigs")
+    private val configFile = File("${configDir.name}/anime.yaml")
+
+    private fun unpackConfig() {
+        if (!configDir.exists())
+            configDir.mkdir()
+        if (!configDir.isDirectory)
+            throw Exception(
+                "${configDir.name} is not a directory! Please, remove the \"${configDir.name}\" file from project's root!"
+            )
+
+        if (configFile.exists())
+            return
+        val configFileRes = this::class.java.getResourceAsStream("/anime.yaml")
+        val buffer = ByteArray(configFileRes.available())
+        configFileRes.read(buffer)
+        val outStream = FileOutputStream(configFile)
+        outStream.write(buffer)
+        outStream.flush()
+        outStream.close()
+    }
+
     override fun botHandler(config: Config): BotHandler {
+        unpackConfig()
         val configLoader = YamlConfigLoaderService<BotConfig>()
-        val configfile = configLoader.configFile("botConfigs/anime", config.profile)
-        val botConfig = configLoader.load(configfile, BotConfig::class.java)
+        val botConfig = configLoader.load(configFile, BotConfig::class.java)
+        configFile.delete()
         return AnitrackerBotHandler(botConfig)
     }
 }
